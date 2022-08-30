5317c58a688e3d8ae61bc46abb011c40cd08be85
Rodrigo Ribeiro <jr.rodrigo.pt@gmail.com>
2022-08-30 18:30:41 +0100

    Initial commit

---
+++ src/main/java/com/rce/reverseadapter/ReverseAdapterApplication.java
@@ -0,0 +1,13 @@
+package com.rce.reverseadapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReverseAdapterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReverseAdapterApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/rce/reverseadapter/Utils.java
@@ -0,0 +1,91 @@
+package com.rce.reverseadapter;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
+
+public class Utils {
+
+    public static ResponseEntity<String> forwardRequest(
+            String scheme,
+            String host,
+            int port,
+            HttpMethod method,
+            String path,
+            Map<String, String> pathParams,
+            Map<String,String> queryParams,
+            Map<String, String> headerParams,
+            String body,
+            MediaType sendType,
+            MediaType receiveType
+    ) {
+        String pathWithVariables = path;
+
+        for (Map.Entry<String,String> entry : pathParams.entrySet()) {
+            pathWithVariables = pathWithVariables.replace("{" + entry.getKey() + "}", entry.getValue());
+        }
+
+        UriComponentsBuilder uri = UriComponentsBuilder
+                .newInstance()
+                .scheme(scheme)
+                .host(host)
+                .port(port)
+                .path(pathWithVariables);
+
+        for (Map.Entry<String,String> entry : queryParams.entrySet()) {
+            uri.queryParam(entry.getKey(),entry.getValue());
+        }
+
+        WebClient.RequestBodySpec requestBodySpec = WebClient.create()
+                .method(method)
+                .uri(uri.build().toUri());
+
+        for (Map.Entry<String, String> entry : headerParams.entrySet()) {
+            requestBodySpec = requestBodySpec.header(entry.getKey(), entry.getValue());
+        }
+
+        if(body != null) {
+            return requestBodySpec
+                    .contentType(sendType)
+                    .accept(receiveType)
+                    .body(BodyInserters.fromValue(body))
+                    .retrieve()
+                    .onStatus(
+                            status -> true,
+                            clientResponse -> Mono.empty()
+                    )
+                    .toEntity(String.class)
+                    .block();
+        } else {
+            return requestBodySpec
+                    .accept(receiveType)
+                    .retrieve()
+                    .onStatus(
+                            status -> true,
+                            clientResponse -> Mono.empty()
+                    )
+                    .toEntity(String.class)
+                    .block();
+        }
+    }
+
+    public static ResponseEntity<String> forwardResponse(int status, HttpHeaders headers, String body) {
+        if(body!=null && !headers.isEmpty()) {
+            return ResponseEntity.status(status).headers(headers).body(body);
+        } else if(body != null) {
+            return ResponseEntity.status(status).body(body);
+        } else if(!headers.isEmpty()) {
+            return ResponseEntity.status(status).headers(headers).build();
+        } else {
+            return ResponseEntity.status(status).build();
+        }
+    }
+
+}
